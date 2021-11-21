dc31709cf701e2e02bbc7c72f00b4825bdec54db
JonahB <jonahb3@gmail.com>
2021-11-20 20:19:19 -0600

    Switched to next.js

---
+++ pages/instagram.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import probe from 'probe-image-size';
+import { GetServerSideProps } from "next";
+import { Navbar } from "../components/Navbar";
+import Head from "next/head";
+import { AccessToken } from "../environment"
+
+
+export default function Instagram({ photos }: { photos: { media_url: string, caption: string, id: string, width: number, height: number }[] }) {
+    return (
+        <div>
+            <Head>
+                <title>Poetry</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <Navbar />
+            <main>
+                {photos.map(({ media_url, caption, id, width, height }) => (
+                    <figure key={id} className="mx-auto p-5 max-w-screen-lg">
+                        <Image src={media_url} layout='responsive' width={width} height={height} alt='Poetry'></Image>
+                        <figcaption className="text-white pt-2">{caption}</figcaption>
+                    </figure>
+                ))}
+            </main>
+        </div>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const res = await fetch(`https://graph.instagram.com/me/media?fields=media_url,caption&access_token=${AccessToken}`);
+    const data = await res.json();
+
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+
+    const photos = await Promise.all(data.data.map(async (data: { media_url: string, caption: string, id: string }) => {
+        let res = await probe(data.media_url);
+        return {
+            ...data,
+            width: res.width,
+            height: res.height
+        }
+    }));
+
+    return {
+        props: {
+            photos
+        }
+    }
+}
